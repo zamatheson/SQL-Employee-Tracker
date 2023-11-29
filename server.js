2941a5a4e8ee52b72be5727fcace67499a4bb0db
Za Matheson <zamatheson@Zas-MacBook-Air.lan>
2023-11-28 18:54:11 -0600

    adding to server.js

--- server.js
+++ server.js
@@ -1,6 +1,22 @@
 const inquirer = require("inquirer");
-const mysql = requireI("mysql2");
-const db = require(" ");
+const mysql = require("mysql2");
+const db = require("./db");
+
+// creates a connection with MySQL
+const connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "EmployeeDBPassword",
+    database: "employeeDB",
+});
+
+// creates connection to the database
+connection.connect((err) =>{
+    if (err) throw err;
+    console.log("You're now connected to the database!");
+    start();
+});
 
 function start() {
     inquirer
@@ -9,39 +25,44 @@ function start() {
             name: "action",
             message: "Choose Your Path...",
             choices: [
-                "Add Employee",
-                "Update Employee Role",
-                "View All Roles",
-                "Add Role",
                 "View All Departments",
-                "Add Department",
+                "View All Roles",
+                "View All Employees",
+                "Add A Department",
+                "Add A Role",
+                "Add An Employee",
+                "Update An Employee Role",
                 "Quit",
             ],
         })
         .then((answer) => {
             switch (answer.action) {
-                case "Add Employee":
-                    viewAddEmployee();
-                    break;
-
-                case "Update Employee Role":
-                    viewUpdateEmployeeRole();
-                    break;
-                    
-                case "View All Roles":
-                    viewAllRoles();
-                    break;
-
-                case "Add Role":
-                    viewAddRole();
-                    break;
-
                 case "View All Departments":
                     viewAllDepartments();
                     break;
-                
-                case "Add Department":
+
+                case "View All Roles":
+                    viewAllRoles();
+                    break;
+                    
+                case "View All Employees":
+                    viewAllEmployees();
+                    break;
+
+                case "Add A Department":
                     viewAddDepartment();
+                    break;
+
+                case "Add A Role":
+                    viewAddRole();
+                    break;
+                
+                case "Add An Employee":
+                    viewAddDepartment();
+                    break;
+
+                case "Update An Employee Role":
+                    viewUpdateEmployeeRole();
                     break;
 
                 case "Quit":
@@ -50,4 +71,37 @@ function start() {
                     break;
             }     
         });
+}
+
+// function for viewing all departments
+function viewAllDepartments() {
+    const query = "SELECT * FROM departments";
+    connection.query(query, (err, res) => {
+        if (err) throw err;
+        console.table(res);
+
+        start();
+    });
+}
+
+// function for viewing all roles
+function viewAllRoles() {
+    const query = "SELECT roles.title, roles.id, departments.department_name, roles.salary roles.department_id = departments.id";
+    connection.query(query, (err, res) => {
+        if (err) throw err;
+        console.table(res);
+
+        start();
+    });
+}
+
+// function for viewing all employees
+function viewAllEmployees() {
+    const query = `SELECT e.id, e.first_name, e.last_name, r.title, d.department_name, r.salary, CONCAT(m.first_name, ' ', m.last_name)`;
+    connection.query(query, (err, res) => {
+        if (err) throw err;
+        console.table(res);
+
+        start();
+    })
 }
